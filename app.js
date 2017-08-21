fada92484d3e6dff234e4eecd464a404119ab7b0
selma <selma@gmail.com>
2017-08-21 17:30:55 +0200

    file static et les erreurs 404 et 500

--- app.js
+++ app.js
@@ -18,4 +18,17 @@ app.get('/toto', function(req, res) {
     res.send('Yo Toto!');
 });
 
+// static file avec express, on essaie de recuperer fichier html dans le dossier /public 
+// avec image qu'on a choisi
 app.use(express.static('public'));
+
+// function pour afficher l'erreur 404 (404.html) la page pas trouvée
+// gerer l'erreur 404
+app.use(function(req, res) {
+    res.status(404);
+    // dirname ça defini le chemin absolue vers le dossier courant avec Node.js
+    // sendFile: C'est le node. js qui travail avec le disque dur
+    res.sendFile(__dirname + '/public/404.html');
+});
+
+// gerer l'erreur 500
