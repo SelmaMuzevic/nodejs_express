42bf4903cee69bc146c5056da2a05935053e67d5
selma <selma@gmail.com>
2017-08-21 11:51:50 +0200

    dans le dossier public recuperation un des fichiers avec images avec express

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+// on recharge toujours le serveur dans le terminal avec node app.js 
+
+
 var express = require('express');
 var app = express();
+
 
 app.get('/', function(req, res) {
     res.send('Hello World!');
@@ -8,3 +12,10 @@ app.get('/', function(req, res) {
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
 });
+
+
+app.get('/toto', function(req, res) {
+    res.send('Yo Toto!');
+});
+
+app.use(express.static('public'));
